0fa4b8f75074dc1eac99a10410c964353e21e64d
Christopher Mendoza <tiptop1112@gmail.com>
2024-10-17 12:41:01 -0700

    Created setMarker function created setMarker function within the gameFlow factory function to represent a players choice when they pick a tile to set their marker on during their turn

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 //create a GameBoard object with gameboard array.
 const gameBoard = (function () {
     const board  = ["","","","","","","","","",];
-    return {board}
+
+    const printBoard = () => {
+        console.log(`
+          ${board[0]} | ${board[1]} | ${board[2]}
+          ---------
+          ${board[3]} | ${board[4]} | ${board[5]}
+          ---------
+          ${board[6]} | ${board[7]} | ${board[8]}
+        `);
+    };
+
+    return {board, printBoard}
 })();
 
 //create factory function that returns an object for the player and 
@@ -11,6 +22,7 @@ const createPlayer = function (name, sign ) {
 
 //create factory function to control flow of game
 const gameFlow = function () {
+    const board = gameBoard.board;
     //create two players and assign the marks they will be using
     const player1 = createPlayer('Player 1', "x");
     const player2 = createPlayer('Player 2', "o");
@@ -21,8 +33,10 @@ const gameFlow = function () {
         currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
         return currentPlayer;
     }
-
-    
+    // set marker on a chosen square by the player
+    const setMarker = function (index, mark) {
+        if (board[index] === "") {
+            board[index] = mark;
+        }
+    }
 }
-
-
